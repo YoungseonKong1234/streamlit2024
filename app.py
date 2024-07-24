d0607196cc0c3fdd8699d892dd3d6676ce596c93
YoungseonKong1234 <ysys.kong@gmail.com>
2024-07-24 11:40:50 +0900

    re

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import random 
 
-st.title('This is a title')
-st.title('_Streamlit_ is :blue[cool] :sunglasses:')
-
+def show() :
+  st.header('주사위 게임', divider='rainbow')
+  clicked = st.button('주사위던지기', type='primary')
+  if clicked :
+    n = random.randint(1, 6)
+    st.image(f'./img/{n}.png')
