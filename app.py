915846339e70e5a4008445a02b57e0813d458bef
YoungseonKong1234 <ysys.kong@gmail.com>
2024-07-24 11:30:22 +0900

    add title

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
+st.title('This is a title')
+st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
